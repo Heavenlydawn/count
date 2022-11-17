457e0f9c0a4bcb7d1b07336ac01f67387171750b
Heavenlydawn <gabrielheavenlydawn@gmail.com>
2022-11-17 10:59:12 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  let [count, setCount] = useState(0);
+  const addCount = () => {
+    setCount(count + 1);
+  };
+
+  const reduceCount = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount((count = 0));
+    // window.location.reload(false)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen flex flex-col justify-center items-center ">
+      <div className="text-center text-9xl my-20"> {count}</div>
+
+     <div>
+     <button
+        onClick={addCount}
+        className="bg-blue-500 p-8 rounded-md text-white text-2xl mx-20 "
+      >
+        Add Count
+      </button>
+      <button
+        onClick={reduceCount}
+        className="bg-blue-500  p-8 rounded-md text-white text-2xl mx-5 "
+      >
+        Reduce Count
+      </button>
+     </div>
+     <div>
+     <button
+        onClick={reset}
+        className="bg-blue-500  p-8 rounded-md text-white text-2xl mx-7 my-10 "
+      >
+        Reset
+      </button>
+     </div>
     </div>
   );
 }
